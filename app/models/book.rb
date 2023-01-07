ead13c40f6739eefb6bc7c32af4022373cd59b2b
Bas Man <bas.man.developer@gmail.com>
2023-01-07 18:43:38 +0000

    created validators for title and author

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,5 @@
+# frozen_string_literal: true
 class Book < ApplicationRecord
+  validates :title, presence: true, length: { minimum: 3 }
+  validates :author, presence: true, length: { minimum: 3 }
 end
