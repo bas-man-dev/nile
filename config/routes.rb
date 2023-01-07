d9c411621afa78b22c67e89cec05e5c12a65ff00
Bas Man <bas.man.developer@gmail.com>
2023-01-07 16:10:13 +0000

    can write a curl command to update book list

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  get 'books/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get '/books' => 'books#index'
+  resources :books, only: %i[index create]
 end
